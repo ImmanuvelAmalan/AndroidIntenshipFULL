a9f9e78142ec1e3b0b4af2d1b61a617cf555aaec
ImmanuvelAmalan <80088192+ImmanuvelAmalan@users.noreply.github.com>
2021-03-05 11:56:22 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,21 @@
+class DataTypes{
+	public static void main(String args[]){
+		int intnum=10;   // int
+		byte bytenum=20; // byte
+		short shortnum=30; // short
+		long longnum=979060634; // long
+		float floatnum=242.23f; //float
+		double doublenum=24214.242345221f; // double
+		boolean bool=true; // boolean
+		char ch='A'; // char
+		System.out.println("Integer: "+intnum);
+		System.out.println("Byte: "+bytenum);
+		System.out.println("Short: "+shortnum);
+		System.out.println("Long: "+longnum);
+		System.out.println("Float: "+floatnum);
+		System.out.println("Double: "+doublenum);
+		System.out.println("Boolean: "+bool);
+		System.out.println("Character: "+ch);
+		
+	}
+}

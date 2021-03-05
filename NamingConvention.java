12e554023159d2cf85100ccc6b8e56b4dd77357c
ImmanuvelAmalan <80088192+ImmanuvelAmalan@users.noreply.github.com>
2021-03-05 12:17:11 +0530

    Add files via upload

---
+++ NamingConvention.java
@@ -0,0 +1,21 @@
+interface Bike{ // Interface with single word
+	void brand();
+}
+interface SportBike{ // Inteface with more than one word
+	void brand();
+} 
+class NamingConvention // Class
+{
+	void speedUp() // Method
+	{
+		System.out.println("Method Call");
+	}
+	
+	public static void main(String args[]) // Method
+	{
+		int MAX_SPEED=80; // Constant Variable
+		int speed=50; // Variable with single word
+		boolean inGear=true; // Variable with more than one word
+	
+	}
+}

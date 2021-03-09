b0bcc7ba23716941a63c14133aab4a2b6b5cbf29
ImmanuvelAmalan <80088192+ImmanuvelAmalan@users.noreply.github.com>
2021-03-09 10:56:03 +0530

    Add files via upload

---
+++ OopsConcepts.java
@@ -0,0 +1,43 @@
+package myclass;
+interface LivingThings{              // ABSTRACTION
+    public boolean isLivingThing();
+    public boolean isNotLivingthing();
+}
+class Animals implements LivingThings{           // CLASS
+    String name;                   // ENCAPSULATION
+    public void eat(String food){
+        System.out.println("Eating flesh");
+    }
+    public void eat(String food1,String food2){   // PLOYMORPHISM - OVERLOADING
+        System.out.println("Eating flesh.. + "+food1+" "+food2);
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {   // METHODS
+        this.name=name;
+    }
+    public boolean isLivingThing(){
+        return true;
+    }
+    public boolean isNotLivingthing() {
+        return false;
+    }
+}
+class Human extends Animals{     // INHERITANCE
+    public String getName() {     // POLYMORPHISM - OVERRIDING
+        return name;
+    }
+    public void setName(String name) {   
+        this.name=name;
+    }
+}
+class OopsConcepts{
+    public static void main(String[] args) {
+        Human human=new Human();    // OBJECTS
+        human.setName("Immanuvel");
+        System.out.println("Name: "+human.getName());
+        human.eat("Pizza","Biriyani");
+        System.out.println(human.isLivingThing());
+    }
+}
